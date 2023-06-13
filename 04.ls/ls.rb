b9489fd34762d8334dcfbabe46b0f091e7607d7c
s-miura7 <130021262+s-miura7@users.noreply.github.com>
2023-06-13 09:50:21 +0900

    パーミッションのメソッドを作成

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -37,22 +37,27 @@ FILE_PERMISSION = {
   '7' => 'rwx'
 }.freeze
 
-def get_details(files_and_directories, options, byte_length, type, permission_type)
+def get_permission(target)
+  permission = ''
+  file_type = File::Stat.new(target).ftype
+  permission += FILE_TYPE[file_type]
+  permission_base_eight = File::Stat.new(target).mode.to_s(8)
+  three_digit = permission_base_eight[(permission_base_eight.length.to_i - 3)...permission_base_eight.to_i]
+  three_digit.each_char { |c| permission += FILE_PERMISSION[c] }
+  permission
+end
+
+def get_details(files_and_directories, options)
   files_and_directories.map! do |files_and_directorie|
     details_files_and_directorie = {}
     if options[:l]
-      permission = ''
-      file_type = File::Stat.new(files_and_directorie).ftype
-      permission += type[file_type]
-      permission_base_eight = File::Stat.new(files_and_directorie).mode.to_s(8)
-      three_digit = permission_base_eight[(permission_base_eight.length.to_i - 3)...permission_base_eight.to_i]
-      three_digit.each_char { |c| permission += permission_type[c] }
+      permission = get_permission(files_and_directorie)
       details_files_and_directorie[:permission] = permission
       file_stat = File::Stat.new(files_and_directorie)
       details_files_and_directorie[:links] = file_stat.nlink.to_s.rjust(file_stat.nlink.to_s.length.to_i + 1)
       details_files_and_directorie[:owner] = Etc.getpwuid(file_stat.uid).name.rjust(Etc.getpwuid(file_stat.uid).name.length.to_i + 1)
       details_files_and_directorie[:group] = Etc.getgrgid(file_stat.gid).name.rjust(Etc.getgrgid(file_stat.gid).name.length.to_i + 1)
-      details_files_and_directorie[:byte_size] = file_stat.size.to_s.rjust(byte_length + 1)
+      details_files_and_directorie[:byte_size] = file_stat.size.to_s.rjust(BYTE_LENGTH + 1)
       details_files_and_directorie[:time] = file_stat.mtime.strftime('%m %d %H:%M').center(file_stat.mtime.strftime('%m %d %H:%M').length + 2)
     end
     details_files_and_directorie[:name] = files_and_directorie
@@ -93,9 +98,9 @@ def output_file(output_num, files_and_directories, options)
 end
 
 temporary_outputs = get_file(directory_path)
-temporary_outputs = get_details(temporary_outputs, params, BYTE_LENGTH, FILE_TYPE, FILE_PERMISSION)
+temporary_outputs = get_details(temporary_outputs, params)
 max_file_length = get_max_length(temporary_outputs)
-# # 一列に出力するファイルの数
+# 一列に出力するファイルの数
 maximum_num = temporary_outputs.length / COLUMNS + 1
 outputs = organizing_arrays(max_file_length, temporary_outputs, COLUMNS, maximum_num)
 output_file(maximum_num, outputs, params)
